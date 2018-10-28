de71029fa1a71da4ac688b2f6c42183b57bf2d3b
John Schoeman <johnschoeman1617@gmail.com>
2018-10-27 20:41:44 -0400

    Scala io

--- projects/08/ProgramFlow/BasicLoop/BasicLoop.asm
+++ projects/08/ProgramFlow/BasicLoop/BasicLoop.asm
@@ -1,132 +1,22 @@
-// push constant 0
-@0
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1
-// pop LCL 0
-@LCL
-D=M
-@0
-D=D+A
-@R13
-M=D
-@SP
-M=M-1
-A=M
-D=M
-@R13
-A=M
-M=D
-(LOOP_START)
-// push ARG 0
-@0
-D=A
-@ARG
-A=D+M
-D=M
-@SP
-A=M
-M=D
-@SP
-M=M+1
-// push LCL 0
-@0
-D=A
-@LCL
-A=D+M
-D=M
-@SP
-A=M
-M=D
-@SP
-M=M+1
-@SP
-M=M-1
-A=M
-D=M
-A=A-1
-M=M+D
-// pop LCL 0	
-@LCL
-D=M
-@0	
-D=D+A
-@R13
-M=D
-@SP
-M=M-1
-A=M
-D=M
-@R13
-A=M
-M=D
-// push ARG 0
-@0
-D=A
-@ARG
-A=D+M
-D=M
-@SP
-A=M
-M=D
-@SP
-M=M+1
-// push constant 1
-@1
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1
-@SP
-M=M-1
-A=M
-D=M
-A=A-1
-M=M-D
-// pop ARG 0
-@ARG
-D=M
-@0
-D=D+A
-@R13
-M=D
-@SP
-M=M-1
-A=M
-D=M
-@R13
-A=M
-M=D
-// push ARG 0
-@0
-D=A
-@ARG
-A=D+M
-D=M
-@SP
-A=M
-M=D
-@SP
-M=M+1
-// if-goto LOOP_START
-@SP
-AM=M-1
-D=M
-@LOOP_START
-D;JNE
-// push LCL 0
-@0
-D=A
-@LCL
-A=D+M
-D=M
-@SP
-A=M
-M=D
-@SP
-M=M+1
+// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/08/ProgramFlow/BasicLoop/BasicLoop.vm
+
+// Computes the sum 1 + 2 + ... + argument[0] and pushes the 
+// result onto the stack. Argument[0] is initialized by the test 
+// script before this code starts running.
+push constant 0    
+pop local 0         // initializes sum = 0
+label LOOP_START
+push argument 0    
+push local 0
+add
+pop local 0	        // sum = sum + counter
+push argument 0
+push constant 1
+sub
+pop argument 0      // counter--
+push argument 0
+if-goto LOOP_START  // If counter > 0, goto LOOP_START
+push local 0
--- projects/08/hack_vm_translator_scala/translator.scala
+++ projects/08/hack_vm_translator_scala/translator.scala
@@ -1,2 +1,19 @@
-println("test")
+import java.io.File
+import java.io.PrintWriter
 
+import scala.io.Source
+
+object Translator {
+   def main(args: Array[String]) {
+      val inputFile: String = "../ProgramFlow/BasicLoop/BasicLoop.vm"
+      val outputFile: String = "../ProgramFlow/BasicLoop/BasicLoop.asm"
+
+      val writer = new PrintWriter(new File(outputFile))
+
+      Source.fromFile(inputFile).foreach {
+        line => writer.write(line)
+      }
+
+      writer.close()
+   }
+}
